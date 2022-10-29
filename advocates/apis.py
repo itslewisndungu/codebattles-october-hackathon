4fe425d9fbc22fd3ac94533827588329220616c1
itslewisndungu <itslewisndungu@gmail.com>
2022-10-29 14:29:09 +0300

    fear: Add search to the advocates API

--- advocates/apis.py
+++ advocates/apis.py
@@ -1,34 +1,35 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
+from rest_framework.generics import get_object_or_404, GenericAPIView
 from .models import Advocate
+from .serializers import AdvocatesSerializer
+from rest_framework import filters
 
 
-class AdvocatesListApi(APIView):
+class AdvocatesListApi(GenericAPIView):
     """
     Get a list of all advocates registered
     """
 
-    class OutputSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Advocate
-            fields = ["profile_pic", "name", "twitter", "username", "bio"]
+    serializer_class = AdvocatesSerializer
+    queryset = Advocate.objects.all()
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["username", "bio", "twitter", "name"]
 
     def get(self, request):
-        qs = Advocate.objects.all()
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+
         res = {
-            "advocates": self.OutputSerializer(qs, many=True).data,
+            "advocates": serializer.data,
         }
+
         return Response(res)
 
 
-class AdvocateRetrieveApi(APIView):
-    class OutputSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Advocate
-            fields = ["profile_pic", "name", "twitter", "username", "bio"]
-
+class AdvocateRetrieveApi(GenericAPIView):
     def get(self, request, id: str):
         qs = Advocate.objects.all()
         advocate = get_object_or_404(qs, id=id)
