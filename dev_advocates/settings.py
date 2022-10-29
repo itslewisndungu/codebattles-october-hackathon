68e2fb171e397719bef79c5b6dd9e454233b7346
itslewisndungu <itslewisndungu@gmail.com>
2022-10-29 11:18:24 +0300

    fix: Add a type to `allowed_hosts`

--- dev_advocates/settings.py
+++ dev_advocates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&^m+eg7%vz8xpw)jafc62&65z9dh09q40y)4nity(3c%ct4e$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
