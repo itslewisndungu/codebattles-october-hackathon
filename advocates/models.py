1c7d69d4d1552547ac287bac63af10cde1bfa20a
itslewisndungu <itslewisndungu@gmail.com>
2022-10-29 12:26:51 +0300

    Create an `advocate` model

--- advocates/models.py
+++ advocates/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Advocate(models.Model):
+    profile_pic = models.CharField(max_length=100, null=True, blank=True)
+    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    twitter = models.CharField(max_length=60)
+    bio = models.TextField(max_length=250, null=True, blank=True)
