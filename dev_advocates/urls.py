7def49151c567251610001ab40f41cc0a4117faf
itslewisndungu <itslewisndungu@gmail.com>
2022-11-08 11:46:57 +0300

    feat: Use namespaces for the urls
    This will enable us to perform `reverse` operations, therefore creating
    `api root` / endpoint

--- dev_advocates/urls.py
+++ dev_advocates/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "advocates": reverse(
+                "advocates:list-create", request=request, format=format
+            ),
+        }
+    )
+
 
 urlpatterns = [
+    path("", api_root),
     path("admin/", admin.site.urls),
     path("auth/", include("rest_framework.urls")),
-    path("advocates/", include("advocates.urls")),
+    path("advocates/", include("advocates.urls", namespace="advocates")),
 ]
